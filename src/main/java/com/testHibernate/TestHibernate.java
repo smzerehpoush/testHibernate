5880953afd842484f6dc51fd4c0f4e15a09964d6
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-04-21 20:05:37 +0430

    Revert "update mysql driver class"
    This reverts commit fe0aafe2

--- src/main/java/com/testHibernate/TestHibernate.java
+++ src/main/java/com/testHibernate/TestHibernate.java
@@ -10,14 +10,15 @@ public class TestHibernate {
         emp.setEmail("demo-user"+ System.currentTimeMillis() +"@mail.com");
         emp.setFirstName("demo");
         emp.setLastName("user");
+        System.out.println("11111111"+emp.getId());
         session.save(emp);
+        System.out.println("22222222-1244"+emp.getId());
         session.getTransaction().commit();
         session = HibernateUtil.getSession();
-        session.getTransaction().begin();
+        session.beginTransaction();
         emp.setEmail("mahdiyar"+System.currentTimeMillis() +"@gmail.com");
         session.saveOrUpdate(emp);
         session.getTransaction().commit();
         HibernateUtil.shutdown();
     }
-
 }
