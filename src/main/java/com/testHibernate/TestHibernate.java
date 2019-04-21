fe0aafe25a078c61567655c256c829ded76d3a5e
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-04-21 20:04:37 +0430

    update mysql driver class

--- src/main/java/com/testHibernate/TestHibernate.java
+++ src/main/java/com/testHibernate/TestHibernate.java
@@ -10,15 +10,14 @@ public class TestHibernate {
         emp.setEmail("demo-user"+ System.currentTimeMillis() +"@mail.com");
         emp.setFirstName("demo");
         emp.setLastName("user");
-        System.out.println("11111111"+emp.getId());
         session.save(emp);
-        System.out.println("22222222-1244"+emp.getId());
         session.getTransaction().commit();
         session = HibernateUtil.getSession();
-        session.beginTransaction();
+        session.getTransaction().begin();
         emp.setEmail("mahdiyar"+System.currentTimeMillis() +"@gmail.com");
         session.saveOrUpdate(emp);
         session.getTransaction().commit();
         HibernateUtil.shutdown();
     }
+
 }
